5b9f53edd1e3f39b0c17dd01ca5a03728c03c25e
Shamanchyk <110655677+Shamanchyk@users.noreply.github.com>
2022-11-09 22:32:13 +0200

    Add files via upload

--- Structural (Структурні)/Composite/script.js
+++ Structural (Структурні)/Composite/script.js
@@ -1,91 +1,68 @@
-abstract class McMenu{
-    protected parent!: McMenu | null;
-
-    public setParent(parent: McMenu | null) {
+"use strict";
+class McMenu {
+    setParent(parent) {
         this.parent = parent;
     }
-
-    public getParent(): McMenu | null {
+    getParent() {
         return this.parent;
     }
-
-    public add(component: McMenu): void { }
-
-    public remove(component: McMenu): void { }
-
-    public isComposite(): boolean {
+    add(component) { }
+    remove(component) { }
+    isComposite() {
         return false;
     }
-
-    public abstract operation(): string;
-
 }
-
-class Hamburger extends McMenu{
-    public operation(): string {
-        return 'Hamburger'
+class Hamburger extends McMenu {
+    operation() {
+        return 'Hamburger';
     }
 }
-
-class Potato extends McMenu{
-    public operation(): string {
-        return 'Big potato'
+class Potato extends McMenu {
+    operation() {
+        return 'Big potato';
     }
 }
-
-class Water extends McMenu{
-    public operation(): string {
-        return 'Coca-cola'
+class Water extends McMenu {
+    operation() {
+        return 'Coca-cola';
     }
 }
-
 class Composite extends McMenu {
-    protected children: McMenu[] = [];
-
-    public add(component: McMenu): void {
+    constructor() {
+        super(...arguments);
+        this.children = [];
+    }
+    add(component) {
         this.children.push(component);
         component.setParent(this);
     }
-
-    public remove(component: McMenu): void {
+    remove(component) {
         const componentIndex = this.children.indexOf(component);
         this.children.splice(componentIndex, 1);
-
         component.setParent(null);
     }
-
-    public isComposite(): boolean {
+    isComposite() {
         return true;
     }
-
-    public operation(): string {
+    operation() {
         const results = [];
         for (const child of this.children) {
             results.push(child.operation());
         }
-
         return `Order (${results.join(' and ')})`;
     }
 }
-
-function clientCode(component: McMenu) {
-
+function clientCode(component) {
     console.log(`RESULT: ${component.operation()}`);
-
 }
-
 const mcMenu = new Composite();
 mcMenu.add(new Hamburger());
-
 const mcMenu2 = new Composite();
 mcMenu2.add(new Hamburger());
 mcMenu2.add(new Potato());
-
 const mcMenu3 = new Composite();
 mcMenu3.add(new Hamburger());
 mcMenu3.add(new Water());
-
 mcMenu.add(mcMenu2);
 mcMenu.add(mcMenu3);
-
-console.log(clientCode(mcMenu))
+console.log(clientCode(mcMenu));
