cb3fc72e741e73aafd1c82bf8b74ffa602c2dcac
Shamanchyk <110655677+Shamanchyk@users.noreply.github.com>
2022-11-14 14:24:28 +0200

    Add files via upload

--- Behavioral (Поведінкові)/Observer/script.js
+++ Behavioral (Поведінкові)/Observer/script.js
@@ -1,59 +1,45 @@
-interface Instagram {
-    subscribe(observer: Observer): void;
-    unSubscribe(observer: Observer): void;
-    notify(): void;
-}
-class ProfileInstagram implements Instagram {
-    private observers: Observer[] = [];
-
-    public subscribe(observer: Observer): void {
+"use strict";
+class ProfileInstagram {
+    constructor() {
+        this.observers = [];
+    }
+    subscribe(observer) {
         console.log('Instagram: На вас підписався користувач...');
         this.observers.push(observer);
     }
-    public unSubscribe(observer: Observer): void {
+    unSubscribe(observer) {
         const observerIndex = this.observers.indexOf(observer);
         if (observerIndex === -1) {
             return console.log('Instagram: Від вас відписався користувач...');
         }
-
         this.observers.splice(observerIndex, 1);
         console.log('Instagram: Ви втратили одного підписника.');
     }
-    public notify(): void {
+    notify() {
         console.log('Instagram: Вам написав ваш підписник...');
         for (const observer of this.observers) {
             observer.update(this);
         }
     }
-    public newMessage(): void {
+    newMessage() {
         console.log('Instagram: Ви маєте не прочитане повідомлення.');
         this.notify();
     }
 }
-
-interface Observer  {
-    update(instagram: Instagram): void;
-}
-class ConcreteObserver1 implements Observer  {
-    public update(instagram: Instagram): void {
-            console.log('ConcreteSubscriber1: Сподобалось ваше Reels.');
+class ConcreteObserver1 {
+    update(instagram) {
+        console.log('ConcreteSubscriber1: Сподобалось ваше Reels.');
     }
 }
-
-class ConcreteObserver2 implements Observer  {
-    public update(instagram: Instagram): void {
-            console.log('ConcreteSubscriber2: Сподобалось ваше Reels.');
+class ConcreteObserver2 {
+    update(instagram) {
+        console.log('ConcreteSubscriber2: Сподобалось ваше Reels.');
     }
 }
-
 const instagram = new ProfileInstagram();
-
 const subscriber1 = new ConcreteObserver1();
 instagram.subscribe(subscriber1);
-
 const subscriber2 = new ConcreteObserver1();
 instagram.subscribe(subscriber2);
-
 instagram.newMessage();
-
 instagram.unSubscribe(subscriber2);
